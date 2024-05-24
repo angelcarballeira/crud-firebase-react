9bac5d632572416dfe2ebe2511e4e759de381c6f
Angel Carballeira <angelcarballeira@gmail.com>
2024-05-24 15:20:02 -0300

    initial structure

---
+++ src/firebaseConfig/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCCkM6yHGrUsRVmq1c-nr0KxITMX8IWGIs',
+  authDomain: 'crud-react-4488c.firebaseapp.com',
+  projectId: 'crud-react-4488c',
+  storageBucket: 'crud-react-4488c.appspot.com',
+  messagingSenderId: '1035620695957',
+  appId: '1:1035620695957:web:37c43fa83bf9b7985cc09c',
+};
+
+const app = initializeApp(firebaseConfig);
